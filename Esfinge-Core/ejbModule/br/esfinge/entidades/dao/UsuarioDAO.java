0e411717bdf817819b5f42850805c2ad67a7d2b3
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-09-18 01:41:06 -0300

    Criada a primeira classe DAO

---
+++ Esfinge-Core/ejbModule/br/esfinge/entidades/dao/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package br.esfinge.entidades.dao;
+
+import br.esfinge.entidades.Usuario;
+import br.esfinge.interfaceDAO.InterfaceDAOImpl;
+
+public class UsuarioDAO extends InterfaceDAOImpl<Usuario, String>{
+
+}
