3b2883eb5a6cbd4b4766a3260b41c109c753a15c
luizdequeiroz <luiz.neto.ads@gmail.com>
2013-10-14 02:13:53 -0300

    Entidade Disciplina refinada.

--- Esfinge-Core/ejbModule/br/esfinge/entidades/Disciplina.java
+++ Esfinge-Core/ejbModule/br/esfinge/entidades/Disciplina.java
@@ -1,74 +1,108 @@
 package br.esfinge.entidades;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
-public class Disciplina implements Serializable {
+@Table(name="tb_disciplina")
+public class Disciplina extends EntidadeGenerica implements Serializable {
 
-	private static final long serialVersionUID = -6317936336226130062L;
+	private static final long serialVersionUID = -8788740074345133591L;
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long idDisciplina;
+	@Column(name="dis_nome", nullable=false)
+	private String nome;
 	
-	@Column(nullable=false)
-	private String nomeDisciplina;
+	@Column(name="dis_descricao", nullable=false)
+	private String descricao;
+
+	@NotNull
+	@ManyToMany(mappedBy="tb_disciplina", fetch=FetchType.LAZY)
+	private Set<Assunto> assuntos;
 	
-	private String descricaoDisciplina;
+	@NotNull
+	@ManyToMany(mappedBy="tb_disciplina", fetch=FetchType.LAZY)
+	private Set<Area> areas;
 
-	public Long getIdDisciplina() {return idDisciplina;}
-	public void setIdDisciplina(Long idDisciplina) {this.idDisciplina = idDisciplina;}
+	public String getNome() {
+		return nome;
+	}
 
-	public String getNomeDisciplina() {return nomeDisciplina;}
-	public void setNomeDisciplina(String nomeDisciplina) {this.nomeDisciplina = nomeDisciplina;}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-	public String getDescricaoDisciplina() {return descricaoDisciplina;}
-	public void setDescricaoDisciplina(String descricaoDisciplina) {this.descricaoDisciplina = descricaoDisciplina;}
-	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Set<Assunto> getAssuntos() {
+		return assuntos;
+	}
+
+	public void setAssuntos(Set<Assunto> assuntos) {
+		this.assuntos = assuntos;
+	}
+
+	public Set<Area> getAreas() {
+		return areas;
+	}
+
+	public void setAreas(Set<Area> areas) {
+		this.areas = areas;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime
-				* result
-				+ ((descricaoDisciplina == null) ? 0 : descricaoDisciplina
-						.hashCode());
+		int result = super.hashCode();
+		result = prime * result + ((areas == null) ? 0 : areas.hashCode());
 		result = prime * result
-				+ ((idDisciplina == null) ? 0 : idDisciplina.hashCode());
+				+ ((assuntos == null) ? 0 : assuntos.hashCode());
 		result = prime * result
-				+ ((nomeDisciplina == null) ? 0 : nomeDisciplina.hashCode());
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		Disciplina other = (Disciplina) obj;
-		if (descricaoDisciplina == null) {
-			if (other.descricaoDisciplina != null)
+		if (areas == null) {
+			if (other.areas != null)
 				return false;
-		} else if (!descricaoDisciplina.equals(other.descricaoDisciplina))
+		} else if (!areas.equals(other.areas))
 			return false;
-		if (idDisciplina == null) {
-			if (other.idDisciplina != null)
+		if (assuntos == null) {
+			if (other.assuntos != null)
 				return false;
-		} else if (!idDisciplina.equals(other.idDisciplina))
+		} else if (!assuntos.equals(other.assuntos))
 			return false;
-		if (nomeDisciplina == null) {
-			if (other.nomeDisciplina != null)
+		if (descricao == null) {
+			if (other.descricao != null)
 				return false;
-		} else if (!nomeDisciplina.equals(other.nomeDisciplina))
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
