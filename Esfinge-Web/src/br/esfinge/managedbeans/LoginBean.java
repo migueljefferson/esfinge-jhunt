3f03ee40fd77d25ed1306384c88b1f46f092051b
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-10-30 10:52:04 -0300

    primeira classe ManagedBean

---
+++ Esfinge-Web/src/br/esfinge/managedbeans/LoginBean.java
@@ -0,0 +1,12 @@
+package br.esfinge.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="loginBean")
+@RequestScoped
+public class LoginBean {
+
+	
+	
+}
