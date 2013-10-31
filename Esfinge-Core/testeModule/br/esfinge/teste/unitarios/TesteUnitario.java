707112ad6396e8a569b13be79515edabe5467cf3
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-10-31 18:00:46 -0300

    teste criado somente para subir o pacote br.esfinge.teste.unitarios

---
+++ Esfinge-Core/testeModule/br/esfinge/teste/unitarios/TesteUnitario.java
@@ -0,0 +1,14 @@
+package br.esfinge.teste.unitarios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteUnitario {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
