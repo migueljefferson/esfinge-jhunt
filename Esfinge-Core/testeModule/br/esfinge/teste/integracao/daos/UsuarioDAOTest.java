c37345b14b7e11676015214b1bbc796879988445
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-10-31 18:02:17 -0300

    testes de integração das classes DAO's criadas

---
+++ Esfinge-Core/testeModule/br/esfinge/teste/integracao/daos/UsuarioDAOTest.java
@@ -0,0 +1,26 @@
+package br.esfinge.teste.integracao.daos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsuarioDAOTest {
+
+	@Before
+	public void init(){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@After
+	public void conclude(){
+		
+	}
+
+}
