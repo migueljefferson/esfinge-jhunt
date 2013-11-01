28334dffd4653186a3e49ed54ab8cc4767d0e7bb
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-11-01 14:55:30 -0300

    classe de teste da UsuarioRN

---
+++ Esfinge-Core/testeModule/br/esfinge/teste/integracao/negocios/UsuarioRNTest.java
@@ -0,0 +1,26 @@
+package br.esfinge.teste.integracao.negocios;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsuarioRNTest {
+
+	@Before
+	public void init(){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@After
+	public void conclude(){
+		
+	}
+
+}
