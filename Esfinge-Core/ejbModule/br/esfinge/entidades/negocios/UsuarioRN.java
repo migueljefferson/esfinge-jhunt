c73ce401f43f3fab418f30565b75db6e222d0ffe
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2013-11-01 14:42:59 -0300

    Primeira classe de regra de negócio

---
+++ Esfinge-Core/ejbModule/br/esfinge/entidades/negocios/UsuarioRN.java
@@ -0,0 +1,33 @@
+package br.esfinge.entidades.negocios;
+
+import java.util.HashSet;
+
+import javax.ejb.Stateless;
+
+import br.esfinge.entidades.Convite;
+import br.esfinge.entidades.Resumo;
+import br.esfinge.entidades.Usuario;
+import br.esfinge.entidades.dao.UsuarioDAO;
+
+@Stateless(mappedName="ejb/UsuarionRN")
+public class UsuarioRN {
+
+	private UsuarioDAO usuarioDAO;
+	
+	public UsuarioRN() {
+		this.usuarioDAO = new UsuarioDAO();
+	}
+	
+	public void criarContaUsuario(Usuario usuario){
+		if (usuario.getAmizades() == null) {
+			usuario.setAmizades(new HashSet<Convite>());
+		}
+		
+		if (usuario.getResumos() == null) {
+			usuario.setResumos(new HashSet<Resumo>());
+		}
+		
+		this.usuarioDAO.salvar(usuario);
+	}
+	
+}
